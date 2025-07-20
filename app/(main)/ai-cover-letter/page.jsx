915f41da884a4b6b9e58a585e84bd516bc9e22f0
Tanvi Bansal <bansaltanvi2004@gmail.com>
2025-07-20 17:59:48 +0530

    UI Fixes

--- app/(main)/ai-cover-letter/page.jsx
+++ app/(main)/ai-cover-letter/page.jsx
@@ -8,7 +8,7 @@ const AICoverLettersPage = async () => {
   const coverLetters = await getCoverLetters();
 
   return (
-    <div>
+    <div className="container mx-auto px-5 py-6">
       <div className="flex flex-col md:flex-row gap-2 items-center justify-between mb-5">
         <h1 className="text-6xl font-bold gradient-title">My Cover Letters</h1>
         <Link href="/ai-cover-letter/new">
